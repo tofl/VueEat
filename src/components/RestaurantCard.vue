afea9c59128c469955a6ab869038186bec360e27
Tom Flitterman <tom.flitterman@hotmail.fr>
2021-03-26 12:24:57 +0100

    Solved bug on Vuetify import in main.js

---
+++ src/components/RestaurantCard.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: 'RestaurantCard'
+  };
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/RestaurantCardList.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="restaurant-list">
+    <v-card
+      :loading="false"
+      max-width="374"
+    >
+      <v-img
+        height="250"
+        src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+      />
+
+      <v-card-title>Cafe Badilico</v-card-title>
+
+      <v-card-text>
+        <v-row
+          align="center"
+          class="mx-0"
+        >
+          <div class="grey--text ml-4">
+            4.5 (413)
+          </div>
+        </v-row>
+
+        <div class="my-4 subtitle-1">
+          $ • Italian, Cafe
+        </div>
+
+        <div>
+          Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio
+          seating.
+        </div>
+
+        <v-divider class="mx-4" />
+
+        <v-card-title>Tonight's availability</v-card-title>
+
+        <v-card-text>
+          <v-chip-group
+            active-class="deep-purple accent-4 white--text"
+            column
+          >
+            <v-chip>5:30PM</v-chip>
+
+            <v-chip>7:30PM</v-chip>
+
+            <v-chip>8:00PM</v-chip>
+
+            <v-chip>9:00PM</v-chip>
+          </v-chip-group>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-btn
+            color="deep-purple lighten-2"
+            text
+          >
+            Reserve
+          </v-btn>
+        </v-card-actions>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RestaurantList',
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
